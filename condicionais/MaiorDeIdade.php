7d1b9b2a9e694703d5edd556744f31b7aba53d44
Breno Lima <brenosobral500@gmail.com>
2026-04-01 19:45:01 -0300

    Condicionais

---
+++ condicionais/MaiorDeIdade.php
@@ -0,0 +1,22 @@
+<?php
+include "MaiorDeIdade.html";
+
+$idade = $_POST['idade'];
+
+if ($idade >= 18){
+
+    echo "Você é maior de idade";
+
+}
+
+else {
+
+    echo "vc é de meno";
+
+}
+
+?>
+
+
+
+
---
+++ condicionais/ParImpar.php
@@ -0,0 +1,16 @@
+<?php
+
+include "ParImpar.html";
+
+$numero = $_POST['par'];
+
+
+if ($numero / 2 == 0){
+    echo "é PAR"
+
+}
+else{
+    echo "N"
+}
+
+?>
