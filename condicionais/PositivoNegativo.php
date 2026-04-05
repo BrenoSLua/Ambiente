042877bc8eb2b3e371c793676ed34148b0d88972
Breno Lima <brenosobral500@gmail.com>
2026-04-05 17:46:23 -0300

    Exercicio 6 feito

--- condicionais/PositivoNegativo.php
+++ condicionais/PositivoNegativo.php
@@ -4,6 +4,12 @@ include "PositivoNegativo.html";
 
 $number = isset ($_POST['number']) ? floatval($_POST['number']) : 0;
 
-
+if ($number >0){
+    echo "<br>Seu número é positivo";
+}elseif ($number <0){
+    echo "<br>Seu número é negativo";
+}else{
+    echo "<br>Seu número é 0";
+}
 
 ?>
