bcf476d068fd30a52b34704d4ad208b7ba43d405
Breno Lima <brenosobral500@gmail.com>
2026-04-05 13:13:33 -0300

    Desconto 1/4 feito

--- condicionais/Desconto.php
+++ condicionais/Desconto.php
@@ -1,9 +1,29 @@
 <?php 
 
-include "Desconto.html"
+include "Desconto.html";
 
 
+$valor = isset ($_POST['valor']) ? floatval($_POST['valor']) : 0;
 
+
+$desconto1 = 5;
+
+$desconto2 = 10;
+
+$desconto3 = 15;
+
+$desconto4 = 20;
+
+
+if ($valor >500 && $valor <= 1000){
+
+     $descontoFinal1 = $valor * ($desconto1 / 100);
+
+
+    echo "<br> Seu valor da sua compra é ". $valor . "<br><br>Valor do seu desconto é ". $desconto1. "%". 
+    "<br><br>Valor total da compra é: ". $valor - $descontoFinal1; 
+
+} 
 
 
 
