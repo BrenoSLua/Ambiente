99b70ba5cea6fd2aa659fb95244da5131c08d050
Breno Lima <brenosobral500@gmail.com>
2026-04-09 09:37:31 -0300

    Fazendo Imc estilização

--- condicionais/Imc.php
+++ condicionais/Imc.php
@@ -1,6 +1,6 @@
 <?php
 
-include "atividade7.html"; 
+include "Imc.html"; 
 
 $peso = $_POST['peso'];
 $altura = $_POST['altura'];
@@ -10,16 +10,16 @@ echo "Seu IMC é: " . number_format($imc, 2)."<br>";
 
 
 if ($imc < 18.5){
-    echo "Classificação: Abaixo do Peso";
+    echo "<p class='resultado'>Classificação: Abaixo do Peso</p>";
 }
 elseif ($imc >=18.5 && $imc <= 24.9){
-    echo "Classificação: Peso Normal";
+    echo "<p class='resultado'>Classificação: Peso Normal</p>";
 }
 elseif ($imc >= 25 && $imc <= 29.90){
-    echo "classificação: Sobrepeso";
+    echo "<p class='resultado'>classificação: Sobrepeso</p>";
 }
 else {
-    echo "Classificação: Obesidade";
+    echo "<p class='resultado'>Classificação: Obesidade</p>";
 }
 
     
