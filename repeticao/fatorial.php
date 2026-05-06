4f0882e6bcc24bcf333a2134bd26cf75383f2bca
Breno Lima <brenosobral500@gmail.com>
2026-05-05 23:20:04 -0300

    Fatorial feito Laços e loops

--- repeticao/fatorial.php
+++ repeticao/fatorial.php
@@ -1,8 +1,19 @@
 <?php
 
+include "fatorial.html";
 
+$texNum = $_POST['texNum'];
 
+$fatorial = 1;
 
+while($texNum > 1){
+
+    $fatorial = $fatorial * $texNum;
+    
+    $texNum--;  
+}
+
+echo "Fatorial: $fatorial";
 
 
 
