1a373376656ded5a66a51247b9e915f7786fc7b1
Breno Lima <brenosobral500@gmail.com>
2026-05-17 10:02:47 -0300

    corrções

--- switchCase/desconto.php
+++ switchCase/desconto.php
@@ -19,14 +19,16 @@ switch(TRUE){
         echo "<p>O valor total: ". 2 * $quantidade ."<p>";
     break;       
     case $cod == 2 and $quantidade >= 7:
-        echo "<p>Você terá 7% de desconto: ". $re1 = (15 * $quantidade) / 100 * 7 ."</p>";
+        $re1 = (15 * $quantidade) / 100 * 7;
+        echo "<p>Você terá 7% de desconto: ". $re1 ."</p>";
         echo "<p><br> O valor total com desconto: ". (15 * $quantidade) - $re1 ."</p>";
         break;
     case $cod == 2 and $quantidade < 7:
         echo "<p><br>O valor total : ". 15 * $quantidade ."</p>";
         break;
       case $cod == 3 and $quantidade >= 7:
-        echo "<p>Você tera 7% de desconto: ". $re2 = (120 * $quantidade) / 100 * 7 ."</p>";
+        $re2 = (120 * $quantidade) / 100 * 7;
+        echo "<p>Você tera 7% de desconto: ". $re2."</p>";
         echo "<p><br> O valor total: ". (120 * $quantidade) - $re2 ."</p>";
         break;
     case $cod == 3 and $quantidade < 7:
