4a1541d8035f0364f6b84ee8b4129c19c13c3196
Breno Lima <brenosobral500@gmail.com>
2026-05-17 10:00:37 -0300

    Exercicio desconto aplicado

--- switchCase/desconto.php
+++ switchCase/desconto.php
@@ -9,15 +9,31 @@ $caderno = 15;
 $caneta = 2;
 $mochila = 120;
 
-switch($cod and $quantidade){
+switch(TRUE){
     case $cod == 1 and $quantidade >= 7:
-        echo "Você terá 7% de desconto: ". $re = (2 * $quantidade) / 100 * 7;
-        echo "<br>O valor total é: ". (2 * $quantidade) - $re;
+        $re = (2 * $quantidade) / 100 * 7;
+        echo "<p>Você terá 7% de desconto: ". $re."</p>";
+        echo "<p><br>O valor total com desconto: ". (2 * $quantidade) - $re ."</p>";
+        break;
     case $cod == 1 and $quantidade < 7:
-        echo "O valor total é: ". 2 * $quantidade;       
+        echo "<p>O valor total: ". 2 * $quantidade ."<p>";
+    break;       
     case $cod == 2 and $quantidade >= 7:
-        echo "Você terá 7% de desconto: ". $re1(15 * $quantidade) / 100 * 7;
-        echo "<br> O valor taotal é: ". (15 * $quantidade) - $re1
+        echo "<p>Você terá 7% de desconto: ". $re1 = (15 * $quantidade) / 100 * 7 ."</p>";
+        echo "<p><br> O valor total com desconto: ". (15 * $quantidade) - $re1 ."</p>";
+        break;
+    case $cod == 2 and $quantidade < 7:
+        echo "<p><br>O valor total : ". 15 * $quantidade ."</p>";
+        break;
+      case $cod == 3 and $quantidade >= 7:
+        echo "<p>Você tera 7% de desconto: ". $re2 = (120 * $quantidade) / 100 * 7 ."</p>";
+        echo "<p><br> O valor total: ". (120 * $quantidade) - $re2 ."</p>";
+        break;
+    case $cod == 3 and $quantidade < 7:
+        echo "<p><br>O valor total é: ". 120 * $quantidade ."</p>";
+        break;
+    default:
+        echo "<p>Código inválido!</p>";
 
 }
 
