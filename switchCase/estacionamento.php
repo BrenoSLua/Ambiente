cc6efb1b8eb2143e8ed989ef5fffd980fd48214a
Breno Lima <brenosobral500@gmail.com>
2026-05-23 16:51:06 -0300

    case carro finish

--- switchCase/estacionamento.php
+++ switchCase/estacionamento.php
@@ -5,29 +5,49 @@ include "estacionamento.html";
 $veiculo = $_POST['veiculo'];
 $vaga = $_POST['vaga'];
 $plano = $_POST['plano'];
-$time = $_POST['time'];
+$time = (int)$_POST['time'];
+
+
+$pagar = 30 + 5 * ($time - 3);
 
 switch($veiculo){
     case "carro":
         if($vaga == "normal"){
-            if($plano == "horas" and $time <= 1){
+            if($plano == "horas" && $time <= 1){
                 echo "Você irá pagar: 10 reais";
             }
-            elseif($plano == "horas" and $time = 3){
+
+            elseif($plano == "horas" && $time <= 3){
                 echo "Você irá pagar: 30 reais ";
             }
-            elseif($plano == "horas" and $time > 3){
-                echo "Você irá pagar: ". ;
+            elseif($plano == "horas" && $time > 3){
+                echo "Você irá pagar: $pagar reais.";
+            }
+            elseif($plano == "diario"){
+                 echo "Você irá pagar: 40 reais.";
+            }else{
+                echo "Você irá pagar: 200 reais.";
             }
 
-
         }
-         else{
-            echo "oi";
+         elseif($vaga == "vip"){
+            if($plano == "horas"){
+                echo "Não sei + o vip";
+            }
+         }else{
+            
          }
       
         
     break;
+
+    case "moto":
+        if($vaga == "horas" && $time <= 1){
+            
+
+        }
+        break;
+
 }
 
 
